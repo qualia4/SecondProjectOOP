44e971f1e2d5a0955bbf2c839aabd82076b58fb9
Maxim Pozdniakov <mpozdniakov@kse.org.ua>
2022-12-21 10:35:32 +0200

    Shelve base implementation

---
+++ Shelve.h
@@ -0,0 +1,32 @@
+#include "Product.h"
+#include "vector"
+#ifndef SECONDPROJECTOOP_SHELVE_H
+#define SECONDPROJECTOOP_SHELVE_H
+
+
+class Shelve {
+    vector<Product*> products;
+    int capacity;
+    int filled;
+public:
+    Shelve(int capacity, int filled){
+        this->capacity = capacity;
+        this->filled = filled;
+    }
+    void addProduct(Product* product){
+        if(capacity <= filled){
+            cout << "There is no place on the shelve" << endl;
+            return;
+        }
+        products.push_back(product);
+    }
+    int getCapacity(){
+        return capacity;
+    }
+    int getFilled(){
+        return filled;
+    }
+};
+
+
+#endif //SECONDPROJECTOOP_SHELVE_H
